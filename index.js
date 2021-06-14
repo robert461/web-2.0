f34d5000fa25115c70793d6847997a284d9e5391
Matthias <>
2021-06-14 22:01:57 +0200

    fixed default id behavior

--- index.js
+++ index.js
@@ -49,7 +49,7 @@
 
     var currentConnectionAccepted = false;
 
-    var useCustomId = true;
+    var useCustomId = false;
     var customIdCharacters = [
         'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 
         't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L',
